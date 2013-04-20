c779432a750c48ad201530d5b93783049fffee91
Karaignee <karen_rooney@hotmail.com>
2013-04-20 13:52:20 +0100

    lab files upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Formval::Application.config.secret_token = '910926e941a4524793ce2680899dc9803aab92406aa969550b46d22b4a0176bed0a1fc40efb43d188583ed32672aba57d9023cbb6de76e758044a2693bbde854'
